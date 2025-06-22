2b1b4aff6731b0867c65db1ab8112a744ff1cd8f
taourirtnoah <ntaourirt@tvz.hr>
2025-06-22 21:37:18 +0200

    i forgor falats :/

--- connect.php
+++ connect.php
@@ -2,10 +2,10 @@
 // Database configuration
 // Use environment variables with sensible defaults
 
-define('DB_HOST', getenv('DB_HOST');
-define('DB_USER', getenv('DB_USER');
-define('DB_PASS', getenv('DB_PASS');
-define('DB_NAME', getenv('DB_NAME');
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASS', getenv('DB_PASS'));
+define('DB_NAME', getenv('DB_NAME'));
 
 try {
     $pdo = new PDO(
